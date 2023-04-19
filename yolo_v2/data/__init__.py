646008b8d7deedb761707f235a7a3b23e6b7bcc7
zjkjzj <wy163zhuj@163.com>
2023-04-19 20:18:02 +0800

    refactor(yolo_v2): remove unused code

--- yolo_v2/data/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2023/4/18 下午2:56
-@file: __init__.py.py
-@author: zj
-@description: 
-"""
--- yolo_v2/model/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2023/4/15 下午4:43
-@file: __init__.py.py
-@author: zj
-@description: 
-"""
