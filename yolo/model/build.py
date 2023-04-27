06372914c9144f50a2d3e7c2ed98d330b4987ae8
zjykzj <wy163zhuj@163.com>
2023-04-27 15:23:30 +0800

    perf(model): update

--- yolo/model/build.py
+++ yolo/model/build.py
@@ -22,25 +22,33 @@ def build_model(args: Namespace, cfg: Dict, device=None):
     else:
         memory_format = torch.contiguous_format
 
-    anchors = torch.tensor(cfg['MODEL']['ANCHORS'])
-    model = YOLOv2(anchors,
-                   num_classes=cfg['MODEL']['N_CLASSES'],
-                   arch=cfg['MODEL']['BACKBONE'],
-                   pretrained=cfg['MODEL']['BACKBONE_PRETRAINED']
-                   ).to(device)
-    model = model.to(memory_format=memory_format, device=device)
+    model_type = cfg['MODEL']['TYPE']
+    if 'YOLOv2' == model_type:
+        anchors = torch.tensor(cfg['MODEL']['ANCHORS'])
+        model = YOLOv2(anchors,
+                       num_classes=cfg['MODEL']['N_CLASSES'],
+                       arch=cfg['MODEL']['BACKBONE'],
+                       pretrained=cfg['MODEL']['BACKBONE_PRETRAINED']
+                       ).to(device)
+    else:
+        raise ValueError(f"{model_type} doesn't supports")
 
+    model = model.to(memory_format=memory_format, device=device)
     return model
 
 
 def build_criterion(cfg: Dict, device=None):
-    anchors = torch.tensor(cfg['MODEL']['ANCHORS'])
-    criterion = YOLOv2Loss(anchors,
-                           num_classes=cfg['MODEL']['N_CLASSES'],
-                           ignore_thresh=cfg['CRITERION']['IGNORE_THRESH'],
-                           coord_scale=cfg['CRITERION']['COORD_SCALE'],
-                           noobj_scale=cfg['CRITERION']['NOOBJ_SCALE'],
-                           obj_scale=cfg['CRITERION']['OBJ_SCALE'],
-                           class_scale=cfg['CRITERION']['CLASS_SCALE'],
-                           ).to(device)
+    loss_type = cfg['CRITERION']['TYPE']
+    if 'YOLOv2Loss' == loss_type:
+        anchors = torch.tensor(cfg['MODEL']['ANCHORS'])
+        criterion = YOLOv2Loss(anchors,
+                               num_classes=cfg['MODEL']['N_CLASSES'],
+                               ignore_thresh=cfg['CRITERION']['IGNORE_THRESH'],
+                               coord_scale=cfg['CRITERION']['COORD_SCALE'],
+                               noobj_scale=cfg['CRITERION']['NOOBJ_SCALE'],
+                               obj_scale=cfg['CRITERION']['OBJ_SCALE'],
+                               class_scale=cfg['CRITERION']['CLASS_SCALE'],
+                               ).to(device)
+    else:
+        raise ValueError(f"{loss_type} doesn't supports")
     return criterion
