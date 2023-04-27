d8c3a79b01e51c99ffd49a3e88a11571975d5266
zjykzj <wy163zhuj@163.com>
2023-04-27 09:56:07 +0800

    test(evaluate): test VOC Evaluate

---
+++ tests/evaluate.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2023/4/27 上午9:35
+@file: vocevaluate.py
+@author: zj
+@description: 
+"""
+import os
+import glob
+
+from tqdm import tqdm
+
+from yolo.data.evaluate.vocevaluator import VOCEvaluator
+from yolo.data.dataset.vocdataset import VOCDataset
+
+
+def test_voc_evaluate():
+    print("=> Test VOC Evaluate")
+
+    root = '../datasets/voc'
+    VOCdevkit_dir = os.path.join(root, 'VOCdevkit')
+    year = 2007
+    split = 'test'
+    val_evaluator = VOCEvaluator(VOCDataset.classes, VOCdevkit_dir, year=year, split=split)
+    print(val_evaluator)
+
+    all_boxes_dict = dict()
+
+    result_dir = 'tests/results/VOC2007/Main/'
+    print(f"=> Read results from {result_dir}\n")
+    res_path_list = glob.glob(os.path.join(result_dir, '*.txt'))
+    for res_path in tqdm(res_path_list):
+        res_name = os.path.splitext(res_path)[0]
+        class_name = res_name.split('_')[-1]
+
+        box_list = list()
+        with open(res_path, 'r') as f:
+            for line in f:
+                if line.strip() == '':
+                    continue
+
+                tmp_list = line.strip().split(' ')
+                assert len(tmp_list) == 6, line
+                image_name, score, x1, y1, x2, y2 = tmp_list[:6]
+                box_list.append([image_name, float(score), float(x1), float(y1), float(x2), float(y2)])
+        assert class_name not in all_boxes_dict.keys()
+        all_boxes_dict[class_name] = box_list
+
+    val_evaluator.all_boxes_dict = all_boxes_dict
+    val_evaluator.result()
+
+
+if __name__ == '__main__':
+    test_voc_evaluate()
