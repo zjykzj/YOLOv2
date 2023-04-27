5cbce4cc9267d311e22c55d831b977a02c646251
zjykzj <wy163zhuj@163.com>
2023-04-27 14:10:10 +0800

    test(model.py): update test_yolov2() / test_yolov2_fast()

--- tests/model.py
+++ tests/model.py
@@ -13,9 +13,10 @@ import numpy as np
 from yolo.model.yolov2 import YOLOv2
 
 
-def t_yolov2():
-    cfg = 'configs/yolov2_default.cfg'
-    with open(cfg, 'r') as f:
+def test_yolov2():
+    cfg_file = 'configs/yolov2_default.cfg'
+    print(f"=> Test {cfg_file}")
+    with open(cfg_file, 'r') as f:
         import yaml
         cfg = yaml.safe_load(f)
 
@@ -25,7 +26,39 @@ def t_yolov2():
     arch = cfg['MODEL']['BACKBONE']
     pretrained = cfg['MODEL']['BACKBONE_PRETRAINED']
     model = YOLOv2(anchors, arch=arch, pretrained=pretrained)
-    print(model)
+    # print(model)
+
+    model = model.to(device)
+
+    print("=> Train")
+    model.train()
+    image_size = cfg['TRAIN']['IMGSIZE']
+    data = torch.randn(1, 3, image_size, image_size)
+    res = model(data.to(device))
+    print("res:", res.shape)
+
+    print("=> Test")
+    model.eval()
+    image_size = cfg['TEST']['IMGSIZE']
+    data = torch.randn(1, 3, image_size, image_size)
+    outputs = model(data.to(device))
+    print("outputs:", outputs.shape)
+
+
+def test_yolov2_fast():
+    cfg_file = 'configs/yolov2_fast.cfg'
+    print(f"=> Test {cfg_file}")
+    with open(cfg_file, 'r') as f:
+        import yaml
+        cfg = yaml.safe_load(f)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+
+    anchors = torch.tensor(cfg['MODEL']['ANCHORS'], dtype=torch.float)
+    arch = cfg['MODEL']['BACKBONE']
+    pretrained = cfg['MODEL']['BACKBONE_PRETRAINED']
+    model = YOLOv2(anchors, arch=arch, pretrained=pretrained)
+    # print(model)
 
     model = model.to(device)
 
@@ -45,4 +78,5 @@ def t_yolov2():
 
 
 if __name__ == '__main__':
-    t_yolov2()
+    test_yolov2()
+    test_yolov2_fast()
