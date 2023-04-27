f2fbfa5cf55fc03b9b72b26bf55429e122b1741f
zjykzj <wy163zhuj@163.com>
2023-04-27 15:52:10 +0800

    fix(yololoss.py): gt_cls_ids = targets[bi][:num_obj][5]
    IndexError: index 5 is out of bounds for dimension 0 with size 4

--- yolo/model/yololoss.py
+++ yolo/model/yololoss.py
@@ -267,7 +267,7 @@ class YOLOv2Loss(nn.Module):
             # [4]: [x_c, y_c, w, h]
             gt_boxes = targets[bi][:num_obj][:4]
             # [num_obj]
-            gt_cls_ids = targets[bi][:num_obj][5]
+            gt_cls_ids = targets[bi][:num_obj][4]
 
             # 放大到网格大小
             gt_boxes[..., 0::2] *= F_size
