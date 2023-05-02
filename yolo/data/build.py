f98e0293e8c4d72e952241752ed7513d5248ea07
zjkjzj <wy163zhuj@163.com>
2023-05-02 22:48:16 +0800

    refactor(data): update

--- yolo/data/build.py
+++ yolo/data/build.py
@@ -6,9 +6,7 @@
 @author: zj
 @description: 
 """
-
 from typing import Dict
-from argparse import Namespace
 
 import os
 import torch
@@ -24,63 +22,36 @@ def build_data(cfg: Dict, data_root: str, is_train: bool = True, is_distributed:
 
     sampler = None
     transform = Transform(cfg, is_train=is_train)
-    collate_fn = torch.utils.data.default_collate
+    dataset_name = cfg['TRAIN']['DATASET_NAME'] if is_train else cfg['TEST']['DATASET_NAME']
+    img_size = cfg['TRAIN']['IMGSIZE'] if is_train else cfg['TEST']['IMGSIZE']
 
-    if is_train:
-        if 'PASCAL VOC' == data_type:
-            train_dataset_name = cfg['TRAIN']['DATASET_NAME']
-            train_img_size = cfg['TRAIN']['IMGSIZE']
-            dataset = VOCDataset(root=data_root,
-                                 name=train_dataset_name,
-                                 train=is_train,
-                                 transform=transform,
-                                 target_transform=None,
-                                 target_size=train_img_size,
-                                 max_det_nums=max_det_num
-                                 )
-        else:
-            raise ValueError(f"{data_type} doesn't supports")
-
-        if is_distributed:
-            sampler = torch.utils.data.distributed.DistributedSampler(dataset)
-
-        dataloader = torch.utils.data.DataLoader(dataset,
-                                                 batch_size=cfg['DATA']['BATCH_SIZE'],
-                                                 shuffle=(sampler is None),
-                                                 num_workers=cfg['DATA']['WORKERS'],
-                                                 sampler=sampler,
-                                                 collate_fn=collate_fn,
-                                                 pin_memory=True
-                                                 )
-
-        return dataloader, sampler
-    else:
-        if 'PASCAL VOC' == data_type:
-            test_dataset_name = cfg['TEST']['DATASET_NAME']
-            test_img_size = cfg['TEST']['IMGSIZE']
-            dataset = VOCDataset(root=data_root,
-                                 name=test_dataset_name,
-                                 train=is_train,
-                                 transform=transform,
-                                 target_transform=None,
-                                 target_size=test_img_size,
-                                 max_det_nums=max_det_num
-                                 )
-
+    evaluator = None
+    if 'PASCAL VOC' == data_type:
+        dataset = VOCDataset(root=data_root,
+                             name=dataset_name,
+                             train=is_train,
+                             transform=transform,
+                             target_transform=None,
+                             target_size=img_size,
+                             max_det_nums=max_det_num
+                             )
+        if not is_train:
             VOCdevkit_dir = os.path.join(data_root, cfg['TEST']['VOC'])
             year = cfg['TEST']['YEAR']
             split = cfg['TEST']['SPLIT']
-            val_evaluator = VOCEvaluator(dataset.classes, VOCdevkit_dir, year=year, split=split)
-        else:
-            raise ValueError(f"{data_type} doesn't supports")
+            evaluator = VOCEvaluator(dataset.classes, VOCdevkit_dir, year=year, split=split)
+    else:
+        raise ValueError(f"{data_type} doesn't supports")
 
-        dataloader = torch.utils.data.DataLoader(dataset,
-                                                 batch_size=cfg['DATA']['BATCH_SIZE'],
-                                                 shuffle=False,
-                                                 num_workers=cfg['DATA']['WORKERS'],
-                                                 sampler=None,
-                                                 collate_fn=collate_fn,
-                                                 pin_memory=True,
-                                                 )
+    if is_distributed:
+        sampler = torch.utils.data.distributed.DistributedSampler(dataset)
 
-        return dataloader, sampler, val_evaluator
+    dataloader = torch.utils.data.DataLoader(dataset,
+                                             batch_size=cfg['DATA']['BATCH_SIZE'],
+                                             shuffle=False,
+                                             num_workers=cfg['DATA']['WORKERS'],
+                                             sampler=sampler,
+                                             pin_memory=True
+                                             )
+
+    return dataloader, sampler, evaluator
