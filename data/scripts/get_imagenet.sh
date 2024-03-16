938755b4e1380b451f3109eaaab0c80c659ffadb
zjkjzj <wy163zhuj@163.com>
2024-03-16 13:53:29 +0800

    perf(get_imagenet.sh): update download url use

--- data/scripts/get_imagenet.sh
+++ data/scripts/get_imagenet.sh
@@ -26,9 +26,9 @@ mkdir -p $d && cd $d
 
 # Download/unzip train
 if [ "$train" == "true" ]; then
-  wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar # download 138G, 1281167 images
-  mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
-  tar -xf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
+#  wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar # download 138G, 1281167 images
+#  mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
+#  tar -xf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
   find . -name "*.tar" | while read NAME; do
     mkdir -p "${NAME%.tar}"
     tar -xf "${NAME}" -C "${NAME%.tar}"
@@ -39,8 +39,8 @@ fi
 
 # Download/unzip val
 if [ "$val" == "true" ]; then
-  wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar # download 6.3G, 50000 images
-  mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xf ILSVRC2012_img_val.tar
+#  wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar # download 6.3G, 50000 images
+#  mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xf ILSVRC2012_img_val.tar
   wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash # move into subdirs
 fi
 
