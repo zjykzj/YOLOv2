724855490ca6d1e37810e320c8efb2e43fb8ba70
zjykzj <wy163zhuj@163.com>
2024-04-17 14:43:32 +0800

    style(create_anchors.py): update anchors for VOC/coco

--- create_anchors.py
+++ create_anchors.py
@@ -34,8 +34,17 @@ def create_dataset(path):
 
 
 if __name__ == '__main__':
+    # na: 5 - anchors: 13,15, 25,38, 62,61, 104,142, 282,273
+    # na: 9 - anchors: 11,12, 15,34, 36,25, 34,63, 82,57, 69,140, 164,123, 150,269, 386,317
     path = '../datasets/coco/train2017.txt'
-    # path = '../datasets/coco/val2017.txt'
+    # na: 5 - anchors: 46,61, 95,137, 255,174, 190,326, 453,371
+    # na: 9 - anchors: 35,54, 85,71, 69,149, 161,144, 133,263, 224,318, 404,211, 345,430, 538,386
+    # path = [
+    #     '../datasets/VOC/images/train2012',
+    #     '../datasets/VOC/images/train2007',
+    #     '../datasets/VOC/images/val2012',
+    #     '../datasets/VOC/images/val2007',
+    # ]
     dataset, imgsz = create_dataset(path)
 
     thr = 4.0
